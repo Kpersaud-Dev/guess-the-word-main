512e339fa5de03b35b8be002c98a2a7d8539221c
kevin_p <kprsd14@gmail.com>
2021-08-03 21:50:18 -0400

    Added validation

--- js/script.js
+++ js/script.js
@@ -18,9 +18,11 @@ const guessedLettersList = document.querySelector('.guessed-letters'),
 // Placeholder Word
 const word = "magnolia";
 
+const guessedLetters = [];
+
 // Replace Letters with Circles
 
-const updateText = function(word) {
+const updateText = word => {
   const wordArray = [];
 
   for (let letter of word) {
@@ -29,15 +31,59 @@ const updateText = function(word) {
   }
 
   wordInProgress.innerText = wordArray.join("");
-
 }
 
-guessButton.addEventListener("click", function(e) {
+// Validate Input
+
+const validateInput = input => {
+  const acceptedLetter = /[a-zA-Z]/;
+
+  if(input.length === 0) {
+     messageParagraph.innerText = "Enter a letter";
+  } else if(input.length > 1) {
+     messageParagraph.innerText = "Enter only 1 letter";
+  } else if(!input.match(acceptedLetter)) {
+     messageParagraph.innerText = "Enter a letter from A - Z";
+  } else {
+    return input;
+  }
+}
+
+const makeGuess = letter => {
+  // Convert letter to uppercase
+  letter = letter.toUpperCase();
+
+  // Check to see if guessedLetters contains letter
+  if(guessedLetters.includes(letter)) {
+    messageParagraph.innerText = "You've already guessed that letter. Try again."
+  } else {
+    guessedLetters.push(letter);
+    console.log(guessedLetters);
+  }
+}
+
+
+// Event Listener
+
+guessButton.addEventListener("click", e => {
   e.preventDefault();
 
+  // Empty Text of Message Element
+  messageParagraph.innerText = "";
+
+  // Value of input element
   let input = inputText.value;
-  console.log(input);
+  
+  // Validate the input
+  const validateCall = validateInput(input);
+
+  if(validateCall) {
+    makeGuess(input);
+  }
+
+  //Empty the input
   input = "";
+  
 })
 
 updateText(word);
